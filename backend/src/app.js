4347a5e651ea5ebb92192c77c38efba11e6606e0
Reem Kamal Ghoniem <reemkamal111@gmail.com>
2024-05-18 22:12:28 +0200

    socket for the backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,8 @@ import chatRouter from './routes/chatsRoute.js'
 import messageRouter from './routes/messageRoute.js'
 import router2 from './routes/protectedRoute.js'
 import cookieParser from "cookie-parser";
+import socket from 'socket.io';
+
 const app = express()
 app.use(cookieParser());
 app.use(express.json());
@@ -20,4 +22,26 @@ app.use(express.json());
  const PORT = process.env.PORT || 3000;
  app.listen(PORT, () => {
  console.log(`Server is running on port ${PORT}`);
+ });
+
+ const io = socket(server, {
+     cors: {
+         origin: "http://localhost:3000",
+         credentials: true,
+     },
+
+ });
+ global.onlineUsers = new Map();
+
+ io.on("connection", (socket) => {
+     global.chatsocket = socket;
+     socket.on("add-user", (userId)=> {
+         onlineUsers.set(userId, socket.id);
+     });
+     socket.on("send-msg", (data) => {
+         const sendUserSocket = onlineUsers.get(data.to);
+         if (sendUserSocket) {
+             socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+         }
+     });
  });
