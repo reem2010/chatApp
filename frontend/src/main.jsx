f13f15764aa57b9a80667b6fc602fc6912bf8908
Reem Kamal Ghoniem <reemkamal111@gmail.com>
2024-05-24 01:10:13 +0200

    Initialize socket connection with user ID from local storage

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { router } from "./router";
 import { RouterProvider } from "react-router-dom";
+import io from 'socket.io-client';
+
+const userId = localStorage.getItem('userId');
+let socket;
+
+if (userId) {
+  
+  socket = io(import.meta.env.VITE_Host, {
+    withCredentials: true,
+    query: { userId: userId },
+  });
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
