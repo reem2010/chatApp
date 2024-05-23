04942916f617b3e0faefbe8eeecbdd2e69dc7a50
emil <edawood997@gmail.com>
2024-05-23 15:53:26 +0300

    add logout button and use chat app word instead chatapp logo temporoy

--- frontend/src/components/home/header.jsx
+++ frontend/src/components/home/header.jsx
@@ -1,10 +1,19 @@
 import "./styles/header.css";
 import { getUser } from "./data/user";
-
+import { useNavigate } from "react-router-dom";
+function logoutAction(nav) {
+  nav('/')
+}
 const HomeHead = () => {
+  const nav = useNavigate();
   return (
     <header id="HEADER">
-      <div className="container"></div>
+      <div className="container">
+  <ul className="nav-bar">
+    <li>Chat app</li>
+    <button className="pointer" onClick={()=> logoutAction(nav)}>logout</button>
+  </ul>
+      </div>
     </header>
   );
 };
